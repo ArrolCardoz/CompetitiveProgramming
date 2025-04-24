1cb2aeb3c798b60f23e18461dcc780e9b3272ac8
Arrol Cardoz <cardozarrol@gmail.com>
2025-04-24 10:58:15 -0600

    howl

---
+++ Kattis/Weekly Club/howl.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string str;
+  cin >> str;
+  cout << "AWAWH";
+  for (int i = 0; i < str.size(); i++) cout << 'O';
+  return 0;
+}
