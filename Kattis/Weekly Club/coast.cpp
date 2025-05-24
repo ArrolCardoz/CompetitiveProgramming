5ff806ef29393f93810c1b58963ff976cd11d3d7
Arrol Cardoz <cardozarrol@gmail.com>
2025-05-24 12:56:59 -0600

    coast attmept 2

--- Kattis/Weekly Club/coast.cpp
+++ Kattis/Weekly Club/coast.cpp
@@ -1,37 +1,61 @@
 #include <bits/stdc++.h>
 using namespace std;
-void dfs(vector<vector<int>> &grid, vector<vector<int>> &visited, int &ans,
-         int row, int col) {
-  cout << row << ' ' << col << ' ' << ans << endl;
+void dfs(vector<vector<int>> &grid, vector<vector<int>> &visitedTemp,
+         vector<vector<int>> &visited, int &ans, int row, int col) {
+  // cout << row << ' ' << col << ' ' << ans << endl;
   visited[row][col] = 1;
+  visitedTemp[row][col] = 1;
+
   int dx[] = {0, 0, 1, -1};
   int dy[] = {1, -1, 0, 0};
   int isLand = grid[row][col];
-  int coastNum = 0;
   int isLake = 1;
 
   // check 4 sides
   for (int i = 0; i < 4; i++) {
     int r = row + dx[i];
     int c = col + dy[i];
+    // cout << row << ' ' << col << '-' << r << ' ' << c << endl;
     if (isLand) {
       if (r > -1 && r < grid.size() && c > -1 && c < grid[0].size() &&
-          !visited[r][c] && grid[r][c])
-        dfs(grid, visited, ans, r, c);
-      else {
+          !visitedTemp[r][c] && grid[r][c])
+        dfs(grid, visitedTemp, visited, ans, r, c);
+
+      else if (r > -1 && r < grid.size() && c > -1 && c < grid[0].size() &&
+               !grid[r][c]) {
+        // visited[r][c] = -1;
+        // cout << r << ' ' << c << endl;
         ans++;
-        if (!grid[r][c]) dfs(grid, visited, ans, r, c);  // check if lake
+        dfs(grid, visitedTemp, visited, ans, r, c);  // check if lake
+      } else if (r <= -1 || r >= grid.size() || c <= -1 ||
+                 c >= grid[0].size()) {
+        // visited[r][c] = -1;
+
+        // cout << r << ' ' << c << endl;
+
+        ans++;
+        // cout << "CHECKKKKK " << r << ' ' << c << endl;
       }
 
     } else {
       // lake check
-      cout << ")";
-      if (r > -1 && r < grid.size() && c > -1 && c < grid[0].size() &&
-          grid[r][c])
+      // cout << ")";
+      if ((r > -1 && r < grid.size() && c > -1 && c < grid[0].size() &&
+           !grid[r][c]) ||
+          (r <= -1 || r >= grid.size() || c <= -1 || c >= grid[0].size())) {
         isLake = 0;
+        // cout << "TESTTTT" << r << ' ' << c << endl;
+      }
     }
   }
-  if (!isLand && isLake) ans--;
+  if (!isLand && isLake) {
+    // cout << row << ' ' << col
+    //      << " is a    lakeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee " << endl;
+    ans--;
+  }
+  // if (isLand) visitedTemp[row][col] = 2;
+  // else
+  //   cout << row << ' ' << col << " is a not lake" << endl;
 }
 
 int main() {
@@ -49,15 +73,21 @@ int main() {
     }
   }
   int ans = 0;
-  cout << "TEST" << endl;
+  // cout << "TEST" << endl;
   for (int i = 0; i < r; i++) {
     for (int j = 0; j < c; j++) {
-      cout << grid[i][j];
-      //   if (grid[i][j] == 1 && !visited[i][j]) {
-      //     dfs(grid, visited, ans, i, j);
-      //   }
+      // cout << grid[i][j];
+      if (grid[i][j] == 1 && !visited[i][j]) {
+        vector<vector<int>> visitedTemp(r, temp);
+        dfs(grid, visitedTemp, visited, ans, i, j);
+        // cout << "ANS CHECK FIRST ISLAND-" << ans << endl;
+        // for (auto &a : visitedTemp) {
+        //   for (auto &b : a) cout << b;
+        //   cout << endl;
+        // }
+      }
     }
-    cout << endl;
+    // cout << endl;
   }
   cout << ans << endl;
   return 0;
