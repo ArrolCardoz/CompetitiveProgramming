6c5bdfcc1c86fbb9ce3fc6e4fff9458051f10931
Arrol Cardoz <cardozarrol@gmail.com>
2025-06-07 23:03:57 -0600

    trik

---
+++ Kattis/Weekly Club/trik.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int currentPos = 0;
+  string input;
+  int change1, change2;
+  cin >> input;
+  for (auto& it : input) {
+    if (it == 'A') {
+      change1 = 0;
+      change2 = 1;
+    } else if (it == 'B') {
+      change1 = 1;
+      change2 = 2;
+    } else if (it == 'C') {
+      change1 = 0;
+      change2 = 2;
+    }
+    if (change1 == currentPos)
+      currentPos = change2;
+    else if (change2 == currentPos)
+      currentPos = change1;
+  }
+  cout << currentPos + 1 << endl;
+  return 0;
+}
