7549ee01c3d86d62e70b8d53e7fadd71de971767
Arrol Cardoz <cardozarrol@gmail.com>
2025-06-09 22:37:36 -0600

    leftbeehind

---
+++ Kattis/Weekly Club/leftbeehind.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int sweet, sour;
+  while (cin >> sweet >> sour) {
+    if (sweet == 0 && sour == 0) {
+      break;
+    }
+    if (sweet + sour == 13)
+      cout << "Never speak again." << endl;
+    else if (sweet == sour)
+      cout << "Undecided." << endl;
+    else if (sweet > sour)
+      cout << "To the convention." << endl;
+    else
+      cout << "Left beehind." << endl;
+  }
+  return 0;
+}
