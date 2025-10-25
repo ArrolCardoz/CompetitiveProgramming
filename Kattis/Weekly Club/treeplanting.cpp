8ccd8a4e9933604c9afb52da3d7290f409577edc
Arrol Cardoz <cardozarrol@gmail.com>
2025-10-24 23:35:30 -0600

    treeplanting

---
+++ Kattis/Weekly Club/treeplanting.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int numTree[10'000'004][4] = {0};
+// 0 is sappling, 1 and 2 is adult and 3 is mature
+
+int const MOD = 1'000'000'007;
+
+void solution() {
+  for (int i = 0; i < 10'000'000; i++) {
+    numTree[i][0] = ((numTree[i - 1][2] + numTree[i - 1][3]) % MOD + 1) % MOD;
+    numTree[i][1] = numTree[i - 1][0];
+    numTree[i][2] = numTree[i - 1][1];
+    numTree[i][3] = numTree[i - 1][2] + numTree[i - 1][3];
+  }
+}
+
+int main() {
+  solution();
+  int n;
+  cin >> n;
+  while (n--) {
+    int input;
+    cin >> input;
+    cout << (numTree[input][1] + numTree[input][2]) % MOD;
+  }
+  return 0;
+}
