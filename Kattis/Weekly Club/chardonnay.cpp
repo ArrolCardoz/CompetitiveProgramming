2f4a557110c2d42124a4a8cf6f540adfe37b013c
Arrol Cardoz <cardozarrol@gmail.com>
2025-11-01 22:37:04 -0600

    chardonny

---
+++ Kattis/Weekly Club/chardonnay.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    if (n==0)cout<<n<<endl;
+    else if (n==7)cout<<n<<endl;
+    else cout<<n+1<<endl;
+    return 0;
+}
