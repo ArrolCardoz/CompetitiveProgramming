8b1211199680f59ba2b64202b78ef0d88525dc67
Arrol Cardoz <cardozarrol@gmail.com>
2025-12-19 22:14:44 -0700

    ValidAnagram

---
+++ Leecode/ValidAnagram.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isAnagram(string s, string t) {
+  sort(s.begin(), s.end());
+  sort(t.begin(), t.end());
+
+  if (s == t) return true;
+  return false;
+}
+
+bool isAnagram2(string s, string t) {
+  sort(s.begin(), s.end());
+  sort(t.begin(), t.end());
+
+  if (s == t) return true;
+  return false;
+}
+
+int main() { return 0; }
