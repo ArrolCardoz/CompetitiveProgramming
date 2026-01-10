e5ef83bf831139fa8be4c3d596ef4242c4f31672
Arrol Cardoz <cardozarrol@gmail.com>
2026-01-10 10:58:12 -0700

    shuffling

--- Kattis/Weekly Club/shuffling.cpp
+++ Kattis/Weekly Club/shuffling.cpp
@@ -1,19 +1,70 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void shiffleIn(string& currentString, string& initialString, int& moves) {
-  if (currentString == initialString && moves > 0) return;
+int cycle(vector<int>& arr2, vector<bool>& visited, int idx) {
+  int e = idx;
+  int counter = 0;
+  visited[idx] = true;
+
+  do {
+    idx = arr2[idx];
+    counter++;
+    visited[idx] = true;
+  } while (idx != e);
+  return counter;
+}
+
+void shuffle(int& num, string& mode) {
+  vector<int> arr1(num, 0);
+  vector<int> arr2;
+  arr2.reserve(num);
+  unordered_map<int, int> idx;
+  // cerr << num << endl;
+  for (int i = 0; i < num; i++) {
+    arr1[i] = i;
+  }
+
+  int p1 = 0, p2 = (num + 1) / 2;
+  if (mode == "in") {
+    p2 = num / 2;
+    swap(p1, p2);
+  }
+  for (int i = 0; i * 2 < num - 1; i++) {
+    arr2.push_back(arr1[p1]);
+    arr2.push_back(arr1[p2]);
+    p1++;
+    p2++;
+  }
+
+  if (num & 1) {
+    arr2.push_back(arr1[p1]);
+    arr2.push_back(arr1[p2]);
+  } else {
+    if (mode == "in")
+      arr2.push_back(arr1[p1]);
+    else
+      arr2.push_back(arr1[p2]);
+  }
+
+  // for (int i = 0; i < num; i++) {
+  //   cerr << arr2[i] << ' ';
+  // }
+  // cerr << endl;
+  vector<bool> visited(num, false);
+  int maxCycle = 0;
+  for (int i = 0; i < num; i++) {
+    if (!visited[i]) {
+      int temp = cycle(arr2, visited, i);
+      if (temp > maxCycle) maxCycle = temp;
+    }
+  }
+  cout << maxCycle << endl;
 }
 
 int main() {
-  int n;
-  cin >> n;
   string mode;
-  string initialString = "";
-  //   string s = 'a';
-  for (int i = 0; i < n; i++) initialString += 'A' + i;
-  cout << initialString << endl;
-
-  if (mode == "in") shiffleIn(initialString, initialString, 0);
+  int num;
+  cin >> num >> mode;
+  shuffle(num, mode);
   return 0;
 }
