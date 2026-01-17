ce3fcadad7da83d38719f61fac6efafe029e2e31
Arrol Cardoz <cardozarrol@gmail.com>
2026-01-16 19:47:11 -0700

    haughtycuisine

---
+++ Kattis/Weekly Club/haughtycuisine.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  string str;
+  cin >> n;
+  cin >> str;
+  cout << 1 << endl << str << endl;
+  return 0;
+}
