fc74f9f91907e2540982f8a77491c5c3671af21a
Arrol Cardoz <cardozarrol@gmail.com>
2026-01-31 11:22:12 -0700

    deathknight2

---
+++ Kattis/Weekly Club/deathknight2.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  int ans = 0;
+  while (n--) {
+    string str;
+    cin >> str;
+    if (str.find("CD") == string::npos) {
+      ans++;
+      //   cerr << str << endl;
+    }
+  }
+  cout << ans << endl;
+  return 0;
+}
