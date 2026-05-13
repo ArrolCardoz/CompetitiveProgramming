bc9f27be70facd0208ba95a2a97ae39921fd5329
Arrol Cardoz <cardozarrol@gmail.com>
2026-05-13 13:02:48 -0600

    combinationlock

--- Kattis/Weekly Club/combinationlock.cpp
+++ Kattis/Weekly Club/combinationlock.cpp
@@ -1,4 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {}
+int main() {
+  int start, n1, n2, n3;
+  while (cin >> start >> n1 >> n2 >> n3 && (start || n1 || n2 || n3)) {
+    int ans = 1080;
+    ans += ((start + (40 - n1)) % 40) * 9;
+    ans += ((n2 + (40 - n1)) % 40) * 9;
+    ans += ((n2 + (40 - n3)) % 40) * 9;
+    cout << ans << endl;
+  }
+  return 0;
+}
