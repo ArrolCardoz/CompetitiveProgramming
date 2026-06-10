e536beedc9cd3936d4d27d67ca04526c930a70c2
Arrol Cardoz <cardozarrol@gmail.com>
2026-06-10 12:48:31 -0600

    sum of the year

--- Kattis/Weekly Club/sumoftheyear.cpp
+++ Kattis/Weekly Club/sumoftheyear.cpp
@@ -1,4 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() { return 0; }
+int main() {
+  // generate all till 1000
+  int ans[1000];
+  ans[0] = 1;
+  ans[1] = 2;
+  for (long long i = 2; i <= 1000; i++) {
+    if (((i * (i + 1)) / 2) * ((i * (i + 1)) / 2) ==
+        (((i * i) * (i + 1) * (i + 1)) / 4))
+      ans[i - 1] = ans[i - 2] + 1;
+  }
+  int n;
+  cin >> n;
+  cout << n << endl;
+  // same equaltion
+  // so just print n
+
+  return 0;
+}
